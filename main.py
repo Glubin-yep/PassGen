0fed092a57f31c1f4b9c48f4a89d5df0136a539b
Dmytro Chyr <88516266+Glubin-yep@users.noreply.github.com>
2022-12-25 23:59:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random as ran
 print("Entry a length for password ")
 length = int(input(""))
 
-symbol = "qwertyuiop[]asdfghjkl;'zxcvbnm,./1234567890-=QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>? !@#$%^&*()_+"
+symbol = "qwertyuiop[]asdfghjkl;'zxcvbnm,./1234567890-=QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>?!@#$%^&*()_+"
 password = ""
 
 for x in range(length):
